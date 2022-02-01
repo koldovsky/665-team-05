1d2373ccbfb65478d8d72f9f6a9027ce2619be9d
metaluga11 <metaluga11@gmail.com>
2022-02-01 15:46:53 +0200

    Added js for open and close form in customization section

--- js/customization-form.js
+++ js/customization-form.js
@@ -1,4 +1,18 @@
+(function(){
+const modalForm = document.querySelector('.customization__form');
+const modalOpen = document.querySelector('.customization__button__button');
+const modalClose = document.querySelector('.close__icon');
 
+modalOpen.onclick = function(){
+    modalForm.style.display = "block";
+}
+modalClose.onclick = function(){
+    modalForm.style.display = "none";
+}
+window.onclick = function(e){
+    if(e.target === modalForm){
+        modalForm.style.display = "none";
+    }
+}
+})();
 
-
- 
