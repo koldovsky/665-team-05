a560d7fdfb58b043a8a3c510dcfecef6bb968c49
ruslanways <ruslanways@gmail.com>
2022-02-04 15:01:56 +0200

    refactoring of Catalog section: Ruslan Mansurov

--- js/catalog-carousel.js
+++ js/catalog-carousel.js
@@ -1,49 +1,51 @@
-(function() {
-    const catalog = [
-        {
-            id: "1",
-            title: "Shirt & Tops",
-            img: "img/catalog_block/girl1.jpg",
-            alt: "girl in shirt",
-            descriptopn: "Light tops and shirts made from natural materials such as linen, silk and others"
-        },
-        {
-            id: "2",
-            title: "Dresses",
-            img: "img/catalog_block/girl2.webp",
-            alt: "girl in dress",
-            descriptopn: "Casual and evening dresses in mini and midi lengths for your mazing looks"
-        },
-        {
-            id: "3",
-            title: "Pants",
-            img: "img/catalog_block/girl3.webp",
-            alt: "girl in pants",
-            descriptopn: "Pants made from natural fabrics and our own collection of jeans that will last you for years"
-        },
-        {
-            id: "4",
-            title: "Accessories",
-            img: "img/catalog_block/girl4.webp",
-            alt: "girl with accessories",
-            descriptopn: "Belts, rings, gloves necklaces and more for the finishing touch to your look"
-        }
-    ];
+(function () {
+  const catalog = [
+    {
+      id: "1",
+      title: "Shirt & Tops",
+      img: "img/catalog_block/girl1.jpg",
+      alt: "girl in shirt",
+      descriptopn:
+        "Light tops and shirts made from natural materials such as linen, silk and others",
+    },
+    {
+      id: "2",
+      title: "Dresses",
+      img: "img/catalog_block/girl2.webp",
+      alt: "girl in dress",
+      descriptopn:
+        "Casual and evening dresses in mini and midi lengths for your mazing looks",
+    },
+    {
+      id: "3",
+      title: "Pants",
+      img: "img/catalog_block/girl3.webp",
+      alt: "girl in pants",
+      descriptopn:
+        "Pants made from natural fabrics and our own collection of jeans that will last you for years",
+    },
+    {
+      id: "4",
+      title: "Accessories",
+      img: "img/catalog_block/girl4.webp",
+      alt: "girl with accessories",
+      descriptopn:
+        "Belts, rings, gloves necklaces and more for the finishing touch to your look",
+    },
+  ];
 
-    function renderCatalog(catalog) {
-        const catalogContainer = document.querySelector('.catalog__body');
-        for (const item of catalog) {
-            catalogContainer.innerHTML+= `
-            <li class="catalog__doby__shirts">
+  function renderCatalog(catalog) {
+    const catalogContainer = document.querySelector(".catalog__body");
+    for (const item of catalog) {
+      catalogContainer.innerHTML += `
+            <div class="catalog__doby__shirts">
             <img src="${item.img}" alt="${item.alt}"/>
             <h4>${item.title}</h4>
             <p>${item.descriptopn}</p>
-            </li>
-            `; 
-        }
+            </div>
+            `;
     }
+  }
 
-    renderCatalog(catalog);
-
+  renderCatalog(catalog);
 })();
- 
