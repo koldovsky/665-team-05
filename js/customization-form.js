3d2c3f8c4b5f8ba8193bfbb0375a63ad1c092f6c
metaluga11 <metaluga11@gmail.com>
2022-02-07 14:05:29 +0200

    Maked customization-form responsive

--- js/customization-form.js
+++ js/customization-form.js
@@ -1,44 +1,42 @@
 (function () {
-    const modalForm = document.querySelector('.customization__form');
-    const modalOpen = document.querySelector('.customization__button__button');
-    const modalClose = document.querySelector('.close__icon');
-    const body = document.querySelector('body');
-    const form = document.querySelector('.form__container__inputs');
+  const modalForm = document.querySelector(".customization__form");
+  const modalOpen = document.querySelector(".customization__button__button");
+  const modalClose = document.querySelector(".close__icon");
+  const body = document.querySelector("body");
+  const form = document.querySelector(".form__container__inputs");
 
-
-    modalOpen.addEventListener('click', () => {
-        modalForm.classList.toggle('open');
-        body.classList.toggle('open')
-    });
-    modalClose.addEventListener('click', () => {
-        modalForm.classList.toggle('open', false);
-        body.classList.toggle('open', false);
-    });
-    window.addEventListener('click', (e) => {
-        if (e.target === modalForm) {
-            modalForm.classList.toggle('open', false);
-            body.classList.toggle('open', false);
-        }
-    });
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const status = document.querySelector('.contact-form-status');
-        const data = new FormData(event.target);
-        try {
-            await fetch(event.target.action, {
-                method: form.method,
-                body: data,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            });
-            status.innerHTML = "Thanks for your request!";
-            form.reset();
-        } catch {
-            status.innerHTML = "Oops! There was a problem requesting your form";
-        }
+  modalOpen.addEventListener("click", () => {
+    modalForm.classList.toggle("open");
+    body.classList.toggle("open");
+  });
+  modalClose.addEventListener("click", () => {
+    modalForm.classList.toggle("open", false);
+    body.classList.toggle("open", false);
+  });
+  window.addEventListener("click", (e) => {
+    if (e.target === modalForm) {
+      modalForm.classList.toggle("open", false);
+      body.classList.toggle("open", false);
     }
-    form.addEventListener("submit", handleSubmit)
-})();
+  });
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const status = document.querySelector(".contact-form-status");
+    const data = new FormData(event.target);
+    try {
+      await fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      status.innerHTML = "Thanks for your request!";
+      form.reset();
+    } catch {
+      status.innerHTML = "Oops! There was a problem requesting your form";
+    }
+  }
+  form.addEventListener("submit", handleSubmit);
+})();
