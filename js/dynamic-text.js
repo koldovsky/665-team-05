fcb74a941c00fc800f92eb6d2f2fb9a62cc567f8
unknown <svetlanastasjuk@ukr.net>
2022-02-08 19:48:50 +0200

    Added dynamic-text

---
+++ js/dynamic-text.js
@@ -0,0 +1,25 @@
+(function() {
+    function printText( el ){
+
+	let letterTimeout = 70
+
+	let text = el.innerHTML
+	let i = 1
+
+	let print__fn = function(){
+
+			if( i <= text.length ){
+				el.innerHTML = text.substr( 0, i );
+				setTimeout( arguments.callee, letterTimeout );
+			}
+
+			i++;
+		}
+
+	print__fn() // init
+};
+
+// init
+let el = document.getElementById( 'dynamic-text' )
+printText( el );
+})()
